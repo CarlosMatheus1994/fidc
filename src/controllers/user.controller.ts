55ce93112a5401180a27c27b9eba4dc201f14a01
Carlos Matheus <carlos@getconnect.com.br>
2022-01-28 09:45:44 -0300

    Criação do endpoint de patch, e get por id

--- src/controllers/user.controller.ts
+++ src/controllers/user.controller.ts
@@ -1,12 +1,10 @@
 // Uncomment these imports to begin using these cool features!
 
 
-import {Filter, repository} from '@loopback/repository';
-import {get, getModelSchemaRef, param, post, requestBody} from '@loopback/rest';
+import {Count, Filter, FilterExcludingWhere, repository, Where} from '@loopback/repository';
+import {get, getModelSchemaRef, param, patch, post, requestBody} from '@loopback/rest';
 import {Users} from '../models';
 import {UsersRepository} from '../repositories';
-
-
 
 // import {inject} from '@loopback/core';
 
@@ -17,7 +15,7 @@ export class UserController {
     public usersRepository: UsersRepository
   ) { }
 
-
+  // Lista todos os usuários cadastrados no banco de dados.
   @get('/users', {
     responses: {
       '200': {
@@ -32,6 +30,7 @@ export class UserController {
     return this.usersRepository.find(filter)
   }
 
+  // Cria um usuário.
   @post('/users', {
     responses: {
       '200': {
@@ -53,6 +52,50 @@ export class UserController {
     users: Omit<Users, 'id'>,
   ): Promise<Users> {
     return this.usersRepository.create(users);
+  }
+
+  // Atualiza informações de um usuário.
+  @patch('/users/{id}', {
+    responses: {
+      '200': {
+        description: 'Users PATCH success',
+        content: {
+          'application/json': {
+            schema: Users
+          }
+        },
+      },
+    },
+  })
+  async updateAll(
+    @requestBody({
+      content: {
+        'application/json': {
+          schema: getModelSchemaRef(Users, {partial: true}),
+        },
+      },
+    })
+    usersUpdate: Users,
+    @param.where(Users) where?: Where<Users>,
+  ): Promise<Count> {
+    return this.usersRepository.updateAll(usersUpdate, where);
+  }
+
+  // Faz o get por id do usuário.
+  @get('/users/{id}', {
+    responses: {
+      '200': {
+        description: 'users get',
+        content: {'application/json': {schema: Users}},
+      },
+    },
+  })
+  async findById(
+    @param.path.number('id') id: number,
+    @param.filter(Users, {exclude: 'where'})
+    filter?: FilterExcludingWhere<Users>,
+  ): Promise<Users> {
+    return this.usersRepository.findById(id);
   }
 
 
